4352acbb68393f8dc1e1abc7db0237a1c7fd51bf
FTFX <hongxuid@live.com>
2018-12-06 23:05:08 +0800

    Config TarvisCI support

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BUILD_TARGET = "-DALGORITHM_LinkedList=ON \
+                -DALGORITHM_SequenceList=ON \
+                -DALGORITHM_Queue=ON \
+                -DALGORITHM_Stack=ON"
+
+mkdir Build
+cd Build
+cmake ${BUILD_TARGET} ..
+make
+make test
