8be347c1b34a061911ce345bd7ae5a045df02181
FTFX <hongxuid@live.com>
2018-12-13 15:45:01 +0800

    Fix CI Build script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-$BUILD_TARGET="-DALGORITHM_LinkedList=ON \
-               -DALGORITHM_SequenceList=ON \
-               -DALGORITHM_Queue=ON \
-               -DALGORITHM_Stack=ON \
-               -DPROJECT_DocEditor=ON"
+BUILD_TARGET="-DALGORITHM_LinkedList=ON
+              -DALGORITHM_SequenceList=ON
+              -DALGORITHM_Queue=ON
+              -DALGORITHM_Stack=ON
+              -DPROJECT_DocEditor=ON"
 
 mkdir Build
 cd Build
