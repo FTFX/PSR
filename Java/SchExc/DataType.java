fd94f8b6ff3e4225f68266c680e90a187771f906
FTFX <hongxuid@live.com>
2019-04-02 00:07:47 +0800

    Add Java/SchExc

---
+++ Java/SchExc/DataType.java
@@ -0,0 +1,22 @@
+public class DataType
+{
+    public static void main(String args[])
+    {
+        int a;
+        byte b;
+        short c;
+        long d;
+        float e;
+        double f;
+        char g;
+        boolean h;
+        System.out.println("int: " + a);
+        System.out.println("byte: " + b);
+        System.out.println("short: " + c);
+        System.out.println("long: " + d);
+        System.out.println("float: " + e);
+        System.out.println("double: " + f);
+        System.out.println("char: " + g);
+        System.out.println("boolean: " + h);
+    }
+}
