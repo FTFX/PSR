55b57a8d4a3c369093e7b2c12b6f8a47ca5f0ef6
FTFX <12870191+FTFX@users.noreply.github.com>
2020-09-11 08:41:09 +0800

    Add Rust

---
+++ Rust/references/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let s = String::from("hello");
+    println!("s: {}", s);
+    let s2 = s;
+    println!("s2: {}", s2);
+    let s = String::from(", world");
+    println!("s: {}", s);
+}
---
+++ Rust/variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let x = 5;
+    println!("{}", x);
+    let mut x = 6;
+    println!("{}", x);
+    x = 7;
+    println!("{}", x);
+}
